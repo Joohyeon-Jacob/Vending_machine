7b6c1353b092807b2878fa983866d0397a9d77cf
Joohyeon <gregzzang777@kaist.ac.kr>
2023-10-30 17:58:50 +0900

    slack_연동_체크

--- create_vending_machine.py
+++ create_vending_machine.py
@@ -5,6 +5,7 @@ import pandas as pd
 # 자판기 수익
 profit = 0
 
+duration = 7 # 물품 판매 기간(7일)
 money = [5000, 1000, 500, 100]
 timezone = 17 # 6시 ~ 23시(17시간)
 items_info = {'b1': [850], 'b2': [1500], 'b3': [2000], 'c1': [5250], 'c2': [5500]}
@@ -14,7 +15,6 @@ customer_choices = {}
 
 for item_info in items_info.values():
     item_info.extend([15, 0])
-    # customer_choices[item] = 0
 
 # items_info --> key: 물품, value: [가격, (남은)물품 개수, 고객 구매 개수]
 print(f'items_info_init: {items_info}')
@@ -28,9 +28,6 @@ class VendingMachine: # 자판기 class
     def __init__(self, money, timezone, items_info):
         self.money = money # 지불 가능한 현금 종류
         self.timezone = timezone # 판매 가능 시간 e.g. 17시간
-        # self.price = price # 물품 가격
-        # self.item_count = item_count # 물품 종류
-        # self.customer_choices = customer_choices # 고객 선택 기록
         self.items_info = items_info
 
     def calculate(self):
